fe900b6f5c6bec626ef0417b832f5aa9b892c203
Millie Wu <87955067+mengyuanwu1@users.noreply.github.com>
2024-05-07 23:52:19 -0400

    add task end sound; add tutorial text to main scene

--- Assets/TaskEnding.cs
+++ Assets/TaskEnding.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class TaskEnding : MonoBehaviour
 {
     public GameObject FinalDoor;
+    
+    public AudioClip clip; // Assign your AudioClip here in the Inspector
+
+    private AudioSource audioSource;
 
     // private string Text = "Task Completed!";
     // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
 
     // Update is called once per frame
     void Update()
@@ -19,5 +24,6 @@ public class TaskEnding : MonoBehaviour
         FinalDoor.SetActive(false);
         // this.transform.parent.gameObject.GetComponent<TaskManager>().taskText.text = Text;
         this.transform.parent.gameObject.GetComponent<TaskManager>().wristMenu.SetActive(true);
+        audioSource.PlayOneShot(clip);
     }
 }
