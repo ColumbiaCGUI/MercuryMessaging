50fc3fc6d882b686fdc06d0ccbdcabf68f45247f
Jace <35653547+Cyno00@users.noreply.github.com>
2024-06-29 13:26:27 -0400

    Link Nodes:

--- Assets/MercuryGraph/MercuryGraphNode.cs
+++ Assets/MercuryGraph/MercuryGraphNode.cs
@@ -7,13 +7,16 @@ using NewGraph;
 [Serializable, Node("#007F00FF", "Mercury")]
 public class MercuryGraphNode : INode
 {
+    // The MercuryGraphable gameObject that this node represents.
+    public MercuryGraphable mercuryGraphable;
+
     // The Port attribute creates visual connectable ports in the graph view to connect to other nodes.
     // it is important to also add the SerializeReference attribute, so that unity serializes this field as a reference.
-    [Port, SerializeReference]
-    public MercuryGraphNode output = null;
-    
+    // [Port, SerializeReference]
+    // public MercuryGraphNode output = null;
+
     // The PortList attribute instructs the graph view to visualize a dynamic list of ports. 
     [PortList, SerializeReference]
-    public List<MercuryGraphNode> dataList;
+    public List<MercuryGraphNode> mercuryOutputs = new List<MercuryGraphNode>();
 
 }
