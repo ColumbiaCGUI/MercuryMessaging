e59d8fc60333105bc4d6dd1455fcf48046d7af78
Jiatuo Zou <jz3331@columbia.edu>
2025-02-06 11:47:08 -0500

    feature: implement basic scenario structure

--- Assets/UserStudy/Scripts/SpawnManager.cs
+++ Assets/UserStudy/Scripts/SpawnManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject pedestrianPrefab; 
+    public GameObject[] carPrefabs; 
+    public Vector3 spawnPos = new Vector3(-40.0f, 0.02400017f, 79.21f); 
+    public List<Vector3> carSpawnPos; 
+    public float startDelay = 2.0f;
+    public float spawnInterval = 10.0f; 
+    private int currentSpawnIndex = 0; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        carSpawnPos.Add(new Vector3(-54.78f, 0.001151287f, 104)); 
+        carSpawnPos.Add(new Vector3(-56.206f, 0.001151287f, 104)); 
+        InvokeRepeating("SpawnPedestrian", startDelay, spawnInterval);
+        InvokeRepeating("spawnCar", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnPedestrian() {
+        Instantiate(pedestrianPrefab, spawnPos, pedestrianPrefab.transform.rotation);
+    }
+
+    public void spawnCar() {
+        int randomPrefabIndex = Random.Range(0, carPrefabs.Length);
+        GameObject car = carPrefabs[randomPrefabIndex];
+        Vector3 spawnPos = carSpawnPos[currentSpawnIndex]; 
+        currentSpawnIndex = (currentSpawnIndex + 1) % 2;
+        Instantiate(car, spawnPos, car.transform.rotation);
     }
 }
